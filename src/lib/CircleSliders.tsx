eabf50f1a752fecd7e0de0fcf78fbf5aa36f760b
unknown <micjanic20@gmail.com>
2024-04-29 00:25:32 -0500

    initial commit

---
+++ src/lib/CircleSliders.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const CircleSliders: FC = () => {
+  return <div>test</div>;
+};
+
+export default CircleSliders;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 8080,
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.jsx'),
+      name: '@micjanic/circle-sliders',
+      fileName: (format) => `my-component.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', '@pixi/react', 'pixi.js'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
   plugins: [react()],
-})
+});
